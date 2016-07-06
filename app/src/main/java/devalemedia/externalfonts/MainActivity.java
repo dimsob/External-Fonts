ece433260d4596e07b881925e9b4d8756c28eb2d
Sobolev D <sobscorp2@mail.ru>
2016-07-07 03:54:34 +0700

    Work App

--- app/src/main/java/devalemedia/externalfonts/MainActivity.java
+++ app/src/main/java/devalemedia/externalfonts/MainActivity.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +22,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Font path
+        String fontPath = "fonts/AVANTE_1.TTF";
+
+        // text view label
+        TextView txtGhost = (TextView) findViewById(R.id.ghost);
+
+        // Loading Font Face
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+
+        // Applying font
+        txtGhost.setTypeface(tf);
+
+
+        ;
     }
 
     @Override
