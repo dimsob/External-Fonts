e151977a8e56dfe8388f63777dabf2b7a0874d3a
Sobolev D <sobscorp2@mail.ru>
2016-07-07 04:21:52 +0700

    dasdasdasd

--- app/src/main/java/devalemedia/externalfonts/MainActivity.java
+++ app/src/main/java/devalemedia/externalfonts/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // Font path
-        String fontPath = "fonts/AVANTE_1.TTF";
+        String fontPath = "drawable/fonts/2PROT__.TTF";
 
         // text view label
         TextView txtGhost = (TextView) findViewById(R.id.ghost);
@@ -38,25 +38,4 @@ public class MainActivity extends AppCompatActivity {
         ;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
